03aa4d3590926bff970bb68c7ddb7ee14464f7d8
Vivek <verma17@uwindsor.ca>
2022-11-01 07:14:04 -0400

    Shifting the editDistances method from Sequences file into the main file and giving proper reference - vivek

--- assignments/src/assignment_two/AssignmentTwoQuestionThree.java
+++ assignments/src/assignment_two/AssignmentTwoQuestionThree.java
@@ -1,6 +1,5 @@
 package assignment_two;
 
-import algorithmDesign.Sequences;
 import assignment_one.DocumentTextExtractionMode;
 import org.jsoup.Jsoup;
 
@@ -176,6 +175,41 @@ public class AssignmentTwoQuestionThree {
     }
 
     /**
+     * Kindly note, this is using the same function as is available in the Sequences.editDistance() class which is a part of the 'Lab5-Advanced Design and Analysis.zip'
+     *
+     * @param word1
+     * @param word2
+     * @return
+     */
+    public static int editDistance(String word1, String word2) {
+        int len1 = word1.length();
+        int len2 = word2.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        for (int i = 0; i <= len1; i++) dp[i][0] = i;
+        for (int j = 0; j <= len2; j++) dp[0][j] = j;
+        //iterate though, and check last char
+        for (int i = 0; i < len1; i++) {
+            char c1 = word1.charAt(i);
+            for (int j = 0; j < len2; j++) {
+                char c2 = word2.charAt(j);
+                //if last two chars equal
+                if (c1 == c2) {
+                    //update dp value for +1 length
+                    dp[i + 1][j + 1] = dp[i][j];
+                } else {
+                    int replace = dp[i][j] + 1;
+                    int insert = dp[i][j + 1] + 1;
+                    int delete = dp[i + 1][j] + 1;
+                    int min = Math.min(replace, insert);
+                    min = Math.min(delete, min);
+                    dp[i + 1][j + 1] = min;
+                }
+            }
+        }
+        return dp[len1][len2];
+    }
+
+    /**
      * Main function of this question which is to compute the overall score and compute the 10 edit distances and store the words against the query words
      *
      * @param queryWords:          the words for which the score and edit distance is to be calculated
@@ -192,7 +226,7 @@ public class AssignmentTwoQuestionThree {
         for (String queryWord : uniqueQueryWords) { //iterate over the unique query words
             List<EditDistanceNode> editDistanceScoreForQueryWordAndHtmlWord = new ArrayList<>(uniqueQueryWords.size()); //declare list of EditDistanceNode type
             uniqueWordsFromHtmlFile.forEach(htmlWord -> { //iterate over all the unique words from html file and compare the query word with this target word
-                int distance = Sequences.editDistance(queryWord, htmlWord); //computed edit distance
+                int distance = editDistance(queryWord, htmlWord); //computed edit distance
                 editDistanceScoreForQueryWordAndHtmlWord.add(new EditDistanceNode(distance, htmlWord)); //add the computed distance and the word to the list
             });
             editDistanceScoreForQueryWordAndHtmlWord.sort(Comparator.comparing(EditDistanceNode::getDistance)); //Sort the above list on the basis of the edit distance.
