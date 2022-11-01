7bd4d8b4f5a1a1ce074788e4102e6ffe1b893bc7
Raj Dattani <66737352+raj4585@users.noreply.github.com>
2022-11-01 02:47:13 -0400

    Update SimilarWord.java

--- assignments/src/assignment_two/SimilarWord.java
+++ assignments/src/assignment_two/SimilarWord.java
@@ -24,7 +24,7 @@ public class SimilarWord {
 	public static void readFile(String fileName) {
 		//Read the Filename
 		try {
-		    BufferedReader in = new BufferedReader(new FileReader("Assignment2/resources/W3C Web Pages/" + fileName + ".htm"));
+		    BufferedReader in = new BufferedReader(new FileReader("assignment/assignment_two/res/W3C_Web_Pages/" + fileName + ".htm"));
 		    String str;
 		    while ((str = in.readLine()) != null) {
 		    	Builder.append(str);
