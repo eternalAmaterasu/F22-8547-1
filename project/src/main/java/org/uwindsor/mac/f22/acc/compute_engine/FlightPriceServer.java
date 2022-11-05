43249e74a9e46047541f37e0a350d0e1a0c285cc
Vivek <verma17@uwindsor.ca>
2022-11-04 21:59:34 -0400

    Introducing the application.yml and the Controller class along with the base boot server class - vivek

---
+++ project/src/main/java/org/uwindsor/mac/f22/acc/compute_engine/FlightPriceServer.java
@@ -0,0 +1,41 @@
+package org.uwindsor.mac.f22.acc.compute_engine;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.time.ZoneId;
+import java.util.TimeZone;
+
+/**
+ * @author Vivek
+ * @since 05/11/22
+ */
+@Slf4j
+@SpringBootApplication
+public class FlightPriceServer {
+
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("EST", ZoneId.SHORT_IDS))); //force setting, because for some reason my jvm runs in IST time
+        SpringApplication.run(FlightPriceServer.class, args);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("org.uwindsor.mac.f22.acc.compute_engine"))
+                .build();
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void startup() {
+        log.info("Startup complete! Swagger should be accessible on http://localhost:3131/swagger-ui/index.html");
+    }
+}
---
+++ project/src/main/java/org/uwindsor/mac/f22/acc/compute_engine/controller/FlightPriceController.java
@@ -0,0 +1,23 @@
+package org.uwindsor.mac.f22.acc.compute_engine.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ * @author Vivek
+ * @since 05/11/22
+ */
+@Slf4j
+@RestController
+@RequestMapping("/flight/price")
+public class FlightPriceController {
+
+    @GetMapping("/time/system")
+    public String getSystemTime() {
+        return (new Date()).toString();
+    }
+}
