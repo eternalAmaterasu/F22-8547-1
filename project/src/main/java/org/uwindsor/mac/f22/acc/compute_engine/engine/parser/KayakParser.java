febb928d18467aef64f881acb212e59e6bb9eccc
Vivek <verma17@uwindsor.ca>
2022-11-22 21:21:21 -0500

    creation of parser module skeleton - vivek

---
+++ project/src/main/java/org/uwindsor/mac/f22/acc/compute_engine/engine/parser/KayakParser.java
@@ -0,0 +1,21 @@
+package org.uwindsor.mac.f22.acc.compute_engine.engine.parser;
+
+import lombok.extern.slf4j.Slf4j;
+import org.uwindsor.mac.f22.acc.compute_engine.model.SearchResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Vivek
+ * @since 23/11/22
+ */
+@Slf4j
+public class KayakParser {
+
+    public static List<SearchResponse> parseKayakData(String data) {
+        List<SearchResponse> list = new ArrayList<>();
+
+        return list;
+    }
+}
