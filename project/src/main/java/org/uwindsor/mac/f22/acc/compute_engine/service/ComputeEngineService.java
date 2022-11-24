ddd9460aa485d36d0029fb4f763d425d0c181ce3
Vivek <verma17@uwindsor.ca>
2022-11-24 11:37:35 -0500

    adding service methods to ComputeEngineService to facilitate the trie searches on codes and names - vivek

--- project/src/main/java/org/uwindsor/mac/f22/acc/compute_engine/service/ComputeEngineService.java
+++ project/src/main/java/org/uwindsor/mac/f22/acc/compute_engine/service/ComputeEngineService.java
@@ -1,10 +1,12 @@
 package org.uwindsor.mac.f22.acc.compute_engine.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.uwindsor.mac.f22.acc.compute_engine.engine.ComputeEngine;
 import org.uwindsor.mac.f22.acc.compute_engine.model.AirportData;
+import org.uwindsor.mac.f22.acc.compute_engine.model.TrieNode;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +17,7 @@ import java.util.stream.Collectors;
  * @author Vivek
  * @since 21/11/22
  */
-
+@Slf4j
 @Component
 public class ComputeEngineService {
 
@@ -28,7 +30,15 @@ public class ComputeEngineService {
     private Map<String, List<AirportData>> cityAndAirportDataMap;
 
     @Autowired
-    private ComputeEngine computeEngine;
+    @Qualifier("trieOfCityCodes")
+    public TrieNode trieCodeRoot;
+
+    @Autowired
+    @Qualifier("trieOfCityNames")
+    public TrieNode trieNameRoot;
+
+    @Autowired
+    public ComputeEngine computeEngine;
 
     public List<String> getTopNNearestCityCodes(String keyword, int topN) {
         return computeEngine.getTopNNearestStrings(keyword, codeAndAirportDataMap.keySet(), topN);
@@ -42,5 +52,17 @@ public class ComputeEngineService {
         return cities.subList(0, Math.min(cities.size(), topN)).stream()
                 .map(AirportData::getCode)
                 .collect(Collectors.toList());
+    }
+
+    public List<String> getAutoCompleteForPrefixOnCodes(String prefix) {
+        List<String> autoCompletedCodes = computeEngine.getAutoCompleteForPrefix(prefix, trieCodeRoot);
+        log.info("Autocompleted values for prefix: {} => {}", prefix, autoCompletedCodes.toString());
+        return autoCompletedCodes;
+    }
+
+    public List<String> getAutoCompleteForPrefixOnNames(String prefix) {
+        List<String> autoCompletedCodes = computeEngine.getAutoCompleteForPrefix(prefix, trieNameRoot);
+        log.info("Autocompleted values for prefix: {} => {}", prefix, autoCompletedCodes.toString());
+        return autoCompletedCodes;
     }
 }
