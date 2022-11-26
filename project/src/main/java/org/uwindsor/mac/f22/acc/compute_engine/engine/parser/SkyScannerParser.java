8975702f65b70205766ef760e793adc78dc00ecd
Vivek <verma17@uwindsor.ca>
2022-11-26 16:19:29 -0500

    Introducing sky scanner parser shell class - vivek

--- project/src/main/java/org/uwindsor/mac/f22/acc/compute_engine/engine/parser/SkyScannerParser.java
+++ project/src/main/java/org/uwindsor/mac/f22/acc/compute_engine/engine/parser/SkyScannerParser.java
@@ -1,9 +1,17 @@
 package org.uwindsor.mac.f22.acc.compute_engine.engine.parser;
 
+import org.uwindsor.mac.f22.acc.compute_engine.model.SearchResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Vivek
  * @since 23/11/22
  */
 public class SkyScannerParser {
 
+    public static List<SearchResponse> parseSkyScannerData(String pageSource) {
+        return new ArrayList<>();
+    }
 }
