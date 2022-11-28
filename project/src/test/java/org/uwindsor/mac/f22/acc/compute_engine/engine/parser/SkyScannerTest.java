08e5ca589d1c94d0be98aef9fb2ba062af87afbc
SilaGopalAneesh <silagopall@gmail.com>
2022-11-27 23:34:38 -0500

    Final Push

---
+++ project/src/test/java/org/uwindsor/mac/f22/acc/compute_engine/engine/parser/SkyScannerTest.java
@@ -0,0 +1,27 @@
+package org.uwindsor.mac.f22.acc.compute_engine.engine.parser;
+
+import org.junit.jupiter.api.Test;
+import org.uwindsor.mac.f22.acc.compute_engine.model.SearchResponse;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+public class SkyScannerTest {
+    @Test
+    void testParseSkyScannerData() {
+        StringBuilder data = new StringBuilder();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("src/test/resources/SkyScannerData1.html"))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                line = line.trim().toLowerCase();
+                data.append(line).append("\n");
+            }
+        } catch (IOException ignored) {
+        }
+
+        List<SearchResponse> searchResponses = SkyScannerParser.parseSkyScannerData(data.toString());
+    }
+}
